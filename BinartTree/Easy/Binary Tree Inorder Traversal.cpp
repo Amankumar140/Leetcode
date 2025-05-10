85324d554555de1dfdedf7634ad13e8883d3503e
Aman Kumar <148572810+Amankumar140@users.noreply.github.com>
2025-05-11 00:36:17 +0530

    Update Binary Tree Inorder Traversal.cpp

--- BinartTree/Easy/Binary Tree Inorder Traversal.cpp
+++ BinartTree/Easy/Binary Tree Inorder Traversal.cpp
@@ -27,3 +27,37 @@ public:
         return inOrder;
     }
 };
+// iterative approach
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inOrder;
+        stack<TreeNode*> s;
+        TreeNode* current = root;
+       
+        while (current != NULL || !s.empty()) {
+            while (current != NULL) {
+                s.push(current);
+                current = current->left;
+            }
+
+            current = s.top();
+            s.pop();
+            inOrder.push_back(current->val);
+            current = current->right;
+        }
+        return inOrder;
+    }
+};
