e52579da8f1b6da4b5c9951464e1f89998f44c4f
Aman Kumar <148572810+Amankumar140@users.noreply.github.com>
2025-05-11 22:39:44 +0530

    Update Binary Tree Postorder Traversal.cpp

--- BinartTree/Easy/Binary Tree Postorder Traversal.cpp
+++ BinartTree/Easy/Binary Tree Postorder Traversal.cpp
@@ -27,3 +27,46 @@ public:
         return postOrder;
     }
 };
+// iterative apporoach
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+     
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+    if (!root) return result;
+
+    stack<TreeNode*> s;
+    TreeNode* current = root;
+    TreeNode* lastVisited = nullptr;
+
+    while (!s.empty() || current) {
+        if (current) {
+            s.push(current);
+            current = current->left;
+        } else {
+            TreeNode* peekNode = s.top();
+            if (peekNode->right && lastVisited != peekNode->right) {
+                current = peekNode->right;
+            } else {
+                result.push_back(peekNode->val);
+                lastVisited = peekNode;
+                s.pop();
+            }
+        }
+    }
+
+    return result;
+    }
+};
