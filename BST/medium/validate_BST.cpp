1b2512866bf834afb4c3f0f2b17f77826839835b
Aman Kumar <148572810+Amankumar140@users.noreply.github.com>
2025-05-14 23:40:23 +0530

    Create validate_BST.cpp

---
+++ BST/medium/validate_BST.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return isBST(root, LONG_MIN, LONG_MAX);
+    }
+
+    bool isBST(TreeNode* node, long minVal, long maxVal) {
+        if (!node) return true;
+
+        if (node->val <= minVal || node->val >= maxVal)
+            return false;
+
+        return isBST(node->left, minVal, node->val) &&
+               isBST(node->right, node->val, maxVal);
+    }
+};
