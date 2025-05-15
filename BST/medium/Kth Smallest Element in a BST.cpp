c155bb8931fb5a240673c4ba4cf4114eb38d10b2
Aman Kumar <148572810+Amankumar140@users.noreply.github.com>
2025-05-16 00:22:53 +0530

    Update Kth Smallest Element in a BST.cpp

--- BST/medium/Kth Smallest Element in a BST.cpp
+++ BST/medium/Kth Smallest Element in a BST.cpp
@@ -26,3 +26,39 @@ public:
         return in[k-1];
     }
 };
+
+
+// SC O(n)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, int& cnt, int& Ksmallest, int k) {
+        if (root == NULL || cnt > k)
+            return;
+        inorder(root->left, cnt, Ksmallest, k);
+        cnt++;
+        if (cnt == k) {
+            Ksmallest = root->val;
+            return;
+        }
+        inorder(root->right, cnt, Ksmallest, k);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        int Ksmallest = 0;
+        int cnt = 0;
+        inorder(root, cnt, Ksmallest, k);
+        return Ksmallest;
+    }
+};
