3d93ca38856b93edc5148d103c61afab9bdbfe1a
Aman Kumar <148572810+Amankumar140@users.noreply.github.com>
2025-05-18 00:47:18 +0530

    Update Sort Characters By Frequency.cpp

--- Strings/Medium/Sort Characters By Frequency.cpp
+++ Strings/Medium/Sort Characters By Frequency.cpp
@@ -14,4 +14,45 @@ public:
         }
         return res;
     }
+
+};
+
+// SC O(n) TCO(n)
+class Solution {
+public:
+    string frequencySort(string s) {
+        vector<int> a(62, 0);
+        for (char c : s) {
+            if (islower(c))
+                a[c - 'a']++;
+            else if (isupper(c))
+                a[c - 'A'+26]++;
+            if (isdigit(c))
+                a[c - '0'+52]++;
+        }
+        string r;
+        while (true) {
+            int m = 0;
+            int idx = -1;
+            for (int i = 0; i < 62; i++) {
+                if (a[i] > m) {
+                    m = a[i];
+                    idx = i;
+                }
+            }
+            if (m == 0)
+                break;
+            char ch;
+            if (idx < 26)
+                ch = 'a' + idx;
+            else if (idx < 52)
+                ch = 'A' + (idx - 26);
+            else
+                ch = '0' + (idx - 52);
+            while (m--)
+                r += ch;
+            a[idx] = 0;
+        }
+        return r;
+    }
 };
