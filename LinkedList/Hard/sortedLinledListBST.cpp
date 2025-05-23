f9d5c57a7abf6dc5c7d571d228868640e1225b4b
Aman Kumar <148572810+Amankumar140@users.noreply.github.com>
2025-05-24 00:13:02 +0530

    Update sortedLinledListBST.cpp

--- LinkedList/Hard/sortedLinledListBST.cpp
+++ LinkedList/Hard/sortedLinledListBST.cpp
@@ -42,3 +42,25 @@ class Solution {
         
     }
 };
+// without space
+  TNode *sortedListToBST(LNode *head) {
+         if(!head) return NULL;
+         if(!head->next) return new TNode(head->data);
+         
+         LNode* fast=head;
+         LNode* slow=head;
+         LNode* prev=new LNode(-1);
+         prev->next=head;
+         while(fast!=NULL && fast->next!=NULL){
+             fast=fast->next->next;
+             slow=slow->next;
+             prev=prev->next;
+             
+         }
+         prev->next=NULL;
+         TNode* node=new TNode(slow->data);
+         node->left=sortedListToBST(head);
+         node->right=sortedListToBST(slow->next);
+         return node;
+        
+    }
